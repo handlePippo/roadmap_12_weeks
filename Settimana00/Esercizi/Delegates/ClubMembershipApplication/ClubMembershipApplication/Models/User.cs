6fc326151073e052b4542dc2669f8215b87c4c6a
Filippo <filippopallianimail@gmail.com>
2025-06-07 00:52:34 +0200

    structured 2nd delegate's excercise

---
+++ Settimana00/Esercizi/Delegates/ClubMembershipApplication/ClubMembershipApplication/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ClubMembershipApplication.Models
+{
+    public sealed class User
+    {
+        private string _password = null!;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; private set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required DateTime DateOfBirth { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Password { get => _password; set => _password = Convert.ToBase64String(Encoding.UTF8.GetBytes(value)); }
+        public required string AddressCity { get; set; }
+        public required string AddressFirstLine { get; set; }
+        public string? AddressSecondLine { get; set; }
+        public required string PostCode { get; set; }
+    }
+}
