ceaf15694cd0d4f27914d8ca4de15f81fa416347
Filippo <filippopallianimail@gmail.com>
2025-08-07 00:27:15 +0200

    UWP app to test events and MVVM

---
+++ Settimana00/Esercizi/Events/EmployeeUWPApp/EmployeeComponent/EmployeeViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace EmployeeComponent
+{
+    public sealed class EmployeeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int Id { get; set; }
+
+        private string _firstName = null!;
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (_firstName == value) return;
+                _firstName = value;
+                NotifyPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName { get; set; } = null!;
+        public decimal AnnualSalary { get; set; }
+        public char Gender { get; set; }
+        public bool IsManager => AnnualSalary > 45000;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Settimana00/Esercizi/Events/EmployeeUWPApp/EmployeeComponent/Employees.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+
+namespace EmployeeComponent
+{
+    public sealed class Employees
+    {
+        public static ObservableCollection<EmployeeViewModel> GetEmployees()
+        {
+            var employees = new ObservableCollection<EmployeeViewModel>
+            {
+                new EmployeeViewModel
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Green",
+                    AnnualSalary = 40000,
+                    Gender = 'm'
+                },
+                new EmployeeViewModel
+                {
+                    Id = 2,
+                    FirstName = "Sally",
+                    LastName = "Jones",
+                    AnnualSalary = 55000,
+                    Gender = 'f'
+                },
+                new EmployeeViewModel
+                {
+                    Id = 3,
+                    FirstName = "Bill",
+                    LastName = "Blog",
+                    AnnualSalary = 30000,
+                    Gender = 'm'
+                },
+                new EmployeeViewModel
+                {
+                    Id = 4,
+                    FirstName = "Jamie",
+                    LastName = "Hopkins",
+                    AnnualSalary = 80000,
+                    Gender = 'm'
+                }
+            };
+
+            return employees;
+        }
+    }
+}
